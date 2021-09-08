4b75d45284fac72dddb820ad90d5436780b1613b
PC <PC@PC-BILGISAYAR>
2021-09-09 00:42:33 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kitap = new MyDictionary<int, string>();
+            kitap.Add(101, "Yabancı");
+            kitap.Add(102, "Dönüşüm");
+            kitap.Add(103, "Veba");
+            kitap.Add(104, "Dava");
+            
+        }
+    }
+}
